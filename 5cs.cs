27c6de8b610940c95c90480b68e8936ce1f09879
محمد جهان بزرگی <118634266+mmdx2@users.noreply.github.com>
2022-11-19 12:59:46 -0800

    Add files via upload

---
+++ 5cs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_Work_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n = Convert.ToInt32(Console.ReadLine());
+
+            SQRT(n);
+        }
+
+        private static void SQRT(int n)
+        {
+            Console.WriteLine(Math.Sqrt(n));
+        }
+    }
+}
