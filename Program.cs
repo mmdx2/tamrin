8bc1546a4f5b265adc9f74656dbd446bf6d395f1
محمد جهان بزرگی <118634266+mmdx2@users.noreply.github.com>
2022-11-19 12:47:47 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n;
+
+            Console.WriteLine("Enter your number: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Power(n);
+        }
+
+        static void Power(int n)
+        {
+            Console.WriteLine(Math.Pow(n, 2));
+            Console.WriteLine(Math.Pow(n, 3));
+        }
+    }
+}
